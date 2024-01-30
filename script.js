47cde3cc0f140525ee7acc469fce40dada22d292
Marcio Tanan <marcio-tanan@hotmail.com>
2024-01-29 21:03:43 -0300

    update js

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoPlayPause = document.getElementById("play-pause")
+const audioCapitulo = document.getElementById("audio-capitulo")
+const numeroCapitulo = 10
+
+function tocarFaixa(){
+    
+}
