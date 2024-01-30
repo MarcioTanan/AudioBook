afc607a0ac5750809b1d5fd777b3af54a867487f
Marcio Tanan <marcio-tanan@hotmail.com>
2024-01-29 21:25:27 -0300

    update js

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const botaoPlayPause = document.getElementById("play-pause")
 const audioCapitulo = document.getElementById("audio-capitulo")
 const numeroCapitulo = 10
+let taTocando = 0
 
 function tocarFaixa(){
-    
+    audioCapitulo.play()
 }
+
+function pausarFaixa(){
+    audioCapitulo.pause()
+}
+
+function tocarOuPausar(){
+    if(taTocando === 0){
+        tocarFaixa()
+        taTocando = 1
+    }else{
+        pausarFaixa()
+        taTocando = 0
+    }
+}
+
+botaoPlayPause.addEventListener('click', tocarOuPausar)
